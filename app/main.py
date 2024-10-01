32efba72692229a1f21b3bb632946dde79104d3a
Timothy Sung <tsung8@gatech.edu>
2024-09-30 21:53:19 -0400

    initial streamlit setup

---
+++ app/main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("CS 4641 Project Proposal - Cybersecurity Analysis") # placeholder name until problem fully defined
+st.write('Hello World')
+
+st.header("Introduction")
+
+st.header("Problem Definition")
+
+st.header("Methods")
+
+st.header("Results and Discussion")
+
+st.header("References")
+
+st.header("Gantt Chart")
+
+st.header("Contribution Table")
